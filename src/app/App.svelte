8f801db479ef9f15d0426d441b73b0188ffdc48c
siddharth <me@siddharthjain.in>
2023-04-05 18:24:20 +0530

    leaflet added

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -1,9 +1,13 @@
 <script>
-	import VizInput from './components/VizInput.svelte';
-	import VizSelector from './components/VizType.svelte';
+	import Choropleth from './Choropleth.svelte';
+	import VizInput from './common/VizInput.svelte';
+	import VizSelector from './common/VizType.svelte';
 </script>
 
 <div class="w-[80%] m-auto p-6 flex items-start justify-center">
 	<VizSelector />
 	<VizInput />
+</div>
+<div class="w-[80%] m-auto p-6 flex items-start justify-center">
+	<Choropleth />
 </div>
---
+++ src/app/Choropleth.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let map;
+
+	onMount(async () => {
+		const L = await import('leaflet');
+		map = L.map('map').setView([49, 12], 13);
+		L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png ', {
+			attribution:
+				'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+			maxZoom: 18
+		}).addTo(map);
+	});
+</script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+		integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+		crossorigin=""
+	/>
+</svelte:head>
+
+<div id="map" class="w-full h-[600px]" />
+
+<style>
+</style>
